d4891604975ae0ea864ca9eeda4a64d535657232
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-19 13:51:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,9 +32,7 @@ end
 
 def initial_round
   # code #initial_round here
-  card1 = deal_card
-  card2 = deal_card
-  total = card1 + card2
+  total = deal_card + deal_card
   return display_card_total(total)
 end
 
