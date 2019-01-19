0541a041211cf4641ee1e2d88fa289c621c18994
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-19 14:17:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -67,8 +67,8 @@ def runner
   welcome 
   total = initial_round
   until total > 21 do 
-    hit?(total)
-    total = display_card_total(total)
+    total = hit?(total)
+    display_card_total(total)
   end
   end_game(final)
 end
